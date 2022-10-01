a9aa75b73347ae65ab37a6e2dafe73ccfd57a2b8
Dominik Rimpf <dev@drimpf.de>
2022-10-01 02:36:17 +0200

    Run after closing last window on macOS

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,12 +7,23 @@
 mod menu;
 
 fn main() {
-  let builder = tauri::Builder::default();
+  let builder = tauri::Builder::default()
+    .build(tauri::generate_context!())
+    .expect("error while running tauri application");
 
   #[cfg(target_os = "macos")]
   let builder = builder.menu(menu::menu());
 
+  #[cfg(not(target_os = "macos"))]
   builder
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .run();
+
+  #[cfg(target_os = "macos")]
+  builder
+    .run(|_app_handle, event| match event {
+      tauri::RunEvent::ExitRequested { api, .. } => {
+        api.prevent_exit();
+      }
+      _ => {}
+    });
 }
